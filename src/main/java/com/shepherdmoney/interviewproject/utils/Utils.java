385ea30455e7e72a64dad801d385c11971457a51
Liu Zhe <lzhe72537@gmail.com>
2024-05-02 22:20:35 -0400

    refined comments

--- src/main/java/com/shepherdmoney/interviewproject/utils/Utils.java
+++ src/main/java/com/shepherdmoney/interviewproject/utils/Utils.java
@@ -2,6 +2,9 @@ package com.shepherdmoney.interviewproject.utils;
 
 public class Utils {
 
+    /**
+     * validates if the parameter is null or empty string
+     */
     public static boolean isValidRequestBodyParam(String param) {
         // parameter should not be null or empty string
         if (param != null && !param.isEmpty()) {
