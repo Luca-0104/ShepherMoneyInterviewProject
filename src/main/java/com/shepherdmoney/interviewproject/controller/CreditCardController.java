4f35e388066b58d7dff28c234c2a4ef31079a83a
Liu Zhe <lzhe72537@gmail.com>
2024-05-02 21:47:56 -0400

    bug fixed: CreditCardController-updateBalanceHistoriesByTransactions

--- src/main/java/com/shepherdmoney/interviewproject/controller/CreditCardController.java
+++ src/main/java/com/shepherdmoney/interviewproject/controller/CreditCardController.java
@@ -163,6 +163,109 @@ public class CreditCardController {
 //        return null;
 //    }
 
+    @PostMapping("/credit-card:update-balance")
+    public ResponseEntity<String> updateBalanceHistoriesByTransactions(@RequestBody UpdateBalancePayload[] payloads) {
+        //TODO: Given a list of transactions, update credit cards' balance history.
+        //      1. For the balance history in the credit card
+        //      2. If there are gaps between two balance dates, fill the empty date with the balance of the previous date
+        //      3. Given the payload `payload`, calculate the balance different between the payload and the actual balance stored in the database
+        //      4. If the different is not 0, update all the following budget with the difference
+        //      For example: if today is 4/12, a credit card's balanceHistory is [{date: 4/12, balance: 110}, {date: 4/10, balance: 100}],
+        //      Given a balance amount of {date: 4/11, amount: 110}, the new balanceHistory is
+        //      [{date: 4/12, balance: 120}, {date: 4/11, balance: 110}, {date: 4/10, balance: 100}]
+        //      Return 200 OK if update is done and successful, 400 Bad Request if the given card number
+        //        is not associated with a card.
+
+        // validate the payload
+        for (UpdateBalancePayload payload : payloads) {
+            // return 400 bad request if any given card number is not associated with a card
+            String creditCardNumber = payload.getCreditCardNumber();
+            if (!creditCardRepository.existsByNumber(creditCardNumber)){
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("not all card numbers associated with a card");
+            }
+            // return 400 bad request if any given traction date is even after today
+            if (payload.getBalanceDate().isAfter(LocalDate.now())) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("not all transaction date valid, there should not be any future transactions");
+            }
+        }
+
+        // iterate through all the transactions
+        for (UpdateBalancePayload payload : payloads) {
+            // destructure
+            String creditCardNumber = payload.getCreditCardNumber();
+            LocalDate balanceDate = payload.getBalanceDate();
+            double balanceAmount = payload.getBalanceAmount();
+
+            // query the credit card instance, it will not be null
+            CreditCard creditCard = creditCardRepository.findByNumber(creditCardNumber).get();
+            Map<LocalDate, BalanceHistory> balanceHistories = creditCard.getBalanceHistories();
+
+            /*
+             *  update the balance history of this card using this transaction
+             */
+            // 1. fill the empty date with the balance of the previous date
+            Optional<LocalDate> firstKey = balanceHistories.keySet().stream().findFirst();
+
+            // if the balance history of this card is empty so far
+            if (firstKey.isEmpty()) {
+                // insert the entry with this transaction info
+                balanceHistories.put(balanceDate, new BalanceHistory(balanceDate, balanceAmount, creditCard));
+
+                // update (insert) an entry of today with the balance
+                if (!balanceHistories.containsKey(LocalDate.now())) {
+                    balanceHistories.put(LocalDate.now(), new BalanceHistory(LocalDate.now(), balanceAmount, creditCard));
+                }
+
+                // go to next traction record
+                creditCard.setBalanceHistories(balanceHistories);
+                creditCardRepository.save(creditCard);
+                continue;
+            }
+
+            // get the first date in the history
+            LocalDate firstDate = firstKey.get();
+
+            // if the given date is even before the first date in history
+            if (balanceDate.isBefore(firstDate)) {
+                // we just need to put that record in the history
+                balanceHistories.put(balanceDate, new BalanceHistory(balanceDate, balanceAmount, creditCard));
+                // then make this date as the first date, before filling the gaps
+                firstDate = balanceDate;
+            }
+
+            // iterate from the first date to today to fill the gaps
+            // each time move forward a day, until today is checked
+            for (LocalDate date = firstDate.plusDays(1); date.isBefore(LocalDate.now().plusDays(1)); date = date.plusDays(1)) {
+                // when there is a gap
+                if (!balanceHistories.containsKey(date)) {
+                    // fill the gap with the previous balance
+                    // in our logic, the value of previous date is guaranteed to be existing
+                    balanceHistories.put(date, new BalanceHistory(date, balanceHistories.get(date.minusDays(1)).getBalance(), creditCard));
+                }
+            }
+
+            // 2. calculate the balance different
+            double diff = balanceAmount - balanceHistories.get(balanceDate).getBalance();
+            // go to next traction record if there is no need to update
+            if (diff == 0) {
+                creditCard.setBalanceHistories(balanceHistories);
+                creditCardRepository.save(creditCard);
+                continue;
+            }
+
+            // 3. update all the following budget with the difference
+            for (LocalDate date = balanceDate; date.isBefore(LocalDate.now().plusDays(1)); date = date.plusDays(1)) {
+                balanceHistories.get(date).setBalance(balanceHistories.get(date).getBalance() + diff);
+            }
+            creditCard.setBalanceHistories(balanceHistories);
+            creditCardRepository.save(creditCard);
+
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body("balance histories updated");
+
+    }
+
 
     // insert balance histories for test
     // should be removed finally
@@ -203,118 +306,6 @@ public class CreditCardController {
         }
 
         return null;
-    }
-
-    @PostMapping("/credit-card:update-balance")
-    public ResponseEntity<String> updateBalanceHistoriesByTransactions(@RequestBody UpdateBalancePayload[] payloads) {
-        //TODO: Given a list of transactions, update credit cards' balance history.
-        //      1. For the balance history in the credit card
-        //      2. If there are gaps between two balance dates, fill the empty date with the balance of the previous date
-        //      3. Given the payload `payload`, calculate the balance different between the payload and the actual balance stored in the database
-        //      4. If the different is not 0, update all the following budget with the difference
-        //      For example: if today is 4/12, a credit card's balanceHistory is [{date: 4/12, balance: 110}, {date: 4/10, balance: 100}],
-        //      Given a balance amount of {date: 4/11, amount: 110}, the new balanceHistory is
-        //      [{date: 4/12, balance: 120}, {date: 4/11, balance: 110}, {date: 4/10, balance: 100}]
-        //      Return 200 OK if update is done and successful, 400 Bad Request if the given card number
-        //        is not associated with a card.
-
-        // validate the payload
-        for (UpdateBalancePayload payload : payloads) {
-            // return 400 bad request if any given card number is not associated with a card
-            String creditCardNumber = payload.getCreditCardNumber();
-            if (!creditCardRepository.existsByNumber(creditCardNumber)){
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("not all card numbers associated with a card");
-            }
-            // return 400 bad request if any given traction date is even after today
-            if (payload.getBalanceDate().isAfter(LocalDate.now())) {
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("not all transaction date valid, there should not be any future transactions");
-            }
-        }
-
-        // iterate through all the transactions
-//        int i = 0; // for test
-        for (UpdateBalancePayload payload : payloads) {
-            // destructure
-            String creditCardNumber = payload.getCreditCardNumber();
-            LocalDate balanceDate = payload.getBalanceDate();
-            double balanceAmount = payload.getBalanceAmount();
-
-            // query the credit card instance, it will not be null
-            CreditCard creditCard = creditCardRepository.findByNumber(creditCardNumber).get();
-            Map<LocalDate, BalanceHistory> balanceHistories = creditCard.getBalanceHistories();
-
-            /*
-             *  update the balance history of this card using this transaction
-             */
-            // 1. fill the empty date with the balance of the previous date
-            Optional<LocalDate> firstKey = balanceHistories.keySet().stream().findFirst();
-
-            // if the balance history of this card is empty so far
-            if (firstKey.isEmpty()) {
-                // insert the entry with this transaction info
-                BalanceHistory bh = new BalanceHistory(balanceDate, balanceAmount, creditCard);
-                balanceHistories.put(balanceDate, bh);
-
-                // update (insert) an entry of today with the balance
-                if (!balanceHistories.containsKey(LocalDate.now())) {
-                    balanceHistories.put(LocalDate.now(), bh);
-                }
-
-                // go to next traction record
-                creditCardRepository.save(creditCard);
-                continue;
-            }
-
-            // get the first date in the history
-            LocalDate firstDate = firstKey.get();
-
-            // if the given date is even before the first date in history
-            if (balanceDate.isBefore(firstDate)) {
-                // we just need to put that record in the history
-                balanceHistories.put(balanceDate, new BalanceHistory(balanceDate, balanceAmount, creditCard));
-                // then make this date as the first date, before filling the gaps
-                firstDate = balanceDate;
-            }
-
-            // iterate from the first date to today to fill the gaps
-            // each time move forward a day, until today is checked
-            for (LocalDate date = firstDate.plusDays(1); date.isBefore(LocalDate.now().plusDays(1)); date = date.plusDays(1)) {
-                // when there is a gap
-                if (!balanceHistories.containsKey(date)) {
-                    // fill the gap with the previous balance
-                    // in our logic, the value of previous date is guaranteed to be existing
-                    balanceHistories.put(date, new BalanceHistory(date, balanceHistories.get(date.minusDays(1)).getBalance(), creditCard));
-                }
-            }
-
-            // 2. calculate the balance different
-            double diff = balanceAmount - balanceHistories.get(balanceDate).getBalance();
-            // go to next traction record if there is no need to update
-            if (diff == 0) {
-                creditCardRepository.save(creditCard);
-                continue;
-            }
-
-            // 3. update all the following budget with the difference
-            for (LocalDate date = balanceDate; date.isBefore(LocalDate.now().plusDays(1)); date = date.plusDays(1)) {
-                balanceHistories.get(date).setBalance(balanceHistories.get(date).getBalance() + diff);
-            }
-            creditCard.setBalanceHistories(balanceHistories);
-            creditCardRepository.save(creditCard);
-
-
-            // for test
-//            System.out.println("-------------------- " + i + " ----------------------");
-//            System.out.println("-------------------- card: " + creditCardNumber + " ----------------------");
-//            i++;
-//            for (Map.Entry<LocalDate, BalanceHistory> entry : balanceHistories.entrySet()) {
-//                System.out.println(entry.getKey() + "-----------" + entry.getValue());
-//            }
-
-        }
-
-        return ResponseEntity.status(HttpStatus.OK).body("balance histories updated");
-
     }
     
 }
